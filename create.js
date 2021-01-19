a9e8460192e3bb4620e534ec5a65270ca212ef06
Rafael Oliveira pires <rafaelo.oliveira2020@gmail.com>
2021-01-19 14:48:31 -0300

    Version 1.1.0

--- create.js
+++ create.js
@@ -33,13 +33,13 @@ const cssCreate = "reset.css"
 const htmlCreate = "index.html"
 const jsCreate = "index.js"
 
-fs.writeFile(`${cssCreate}`, "*{\n margin:0%; \n padding:0%; \n box-sizing: border-box; \n} \n html,body{\n width: 100%; \n height:100%; \n }", err =>{
+fs.writeFile(`${cssCreate}`, "*{\n  margin:0%; \n  padding:0%; \n  box-sizing: border-box; \n} \nhtml,body{\n  width: 100%; \n  height:100%; \n }", err =>{
     if(err) throw err;
 
     console.log(`File ${cssCreate} created `);
 })
 
-fs.writeFile(`${htmlCreate}`, `<!DOCTYPE html> \n <html lang="en"> \n <head> \n <meta charset="UTF-8"> \n <meta name="viewport" content="width=devicec-width, initial-scale=1.0"> \n <title>Document</title> \n </head> \n <body> \n </body> \n </html>`, err => {
+fs.writeFile(`${htmlCreate}`, `<!DOCTYPE html> \n<html lang="en"> \n<head> \n    <meta charset="UTF-8"> \n    <meta name="viewport" content="width=devicec-width, initial-scale=1.0"> \n    <link rel="stylesheet" href="css/reset.css"> \n    <link rel="stylesheet" href="css/Style.css">\n<title>Document</title> \n</head> \n<body>\n    <script src="js/index.js"></script> \n</body> \n</html>`, err => {
     if(err) throw err;
 
     console.log(`file ${htmlCreate} created`)
@@ -52,19 +52,19 @@ fs.writeFile(`${jsCreate}`, `window.onload(function(){\n\n\n})`, err =>{
 })
 
 
-fs.rename(`${htmlCreate}` , `project/assets/${htmlCreate}`, err =>{
+fs.rename(`${htmlCreate}` , `assets/${htmlCreate}`, err =>{
     if(err) throw err
 })
 
-fs.rename(`${jsCreate}` , `project/assets/js/${jsCreate}`, err =>{
+fs.rename(`${jsCreate}` , `assets/js/${jsCreate}`, err =>{
     if(err) throw err
 })
 
-fs.rename(`${cssCreate}` , `project/assets/css/${cssCreate}`, err =>{
+fs.rename(`${cssCreate}` , `assets/css/${cssCreate}`, err =>{
     if(err) throw err
 })
 
-fs.rename(`style.css` , `project/assets/css/Style.css`, err =>{
+fs.rename(`style.css` , `assets/css/Style.css`, err =>{
     if(err) throw err
 })
 
